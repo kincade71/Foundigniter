01bf16188cddb483ce9b85e0c3797f4ecbf56197
Kincade71 <kincade71@gmail.com>
2013-07-01 20:35:10 -0400

    Added Foundation 4 removed Boostrap

---
+++ application/views/main/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?= $title ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Richard Robinson http://www.richardorobinson.com">
+
+    <!-- Le styles -->
+    <link href="<?= base_url()?>css/normalize.css" rel="stylesheet">
+    <link href="<?= base_url()?>css/foundation.min.css" rel="stylesheet">
+
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="<?= base_url()?>ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?= base_url()?>ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?= base_url()?>ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?= base_url()?>ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?= base_url()?>ico/apple-touch-icon-57-precomposed.png">
+	<script type="text/javascript" src="<?= base_url(); ?>js/bootstrap.js"></script>
+  </head>
+
+  <body>
